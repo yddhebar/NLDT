56499206bcc92265510240532b49a9273257b0e7
yddhebar <yddhebar@gmail.com>
2023-08-08 00:25:37 -0400

    simplifying view_tree;

--- main_codes/view_tree.py
+++ main_codes/view_tree.py
@@ -6,59 +6,12 @@ from iai_utils import decision_tree_funcs as dt_funcs
 import pickle
 import time
 
-n_links = 6
-if n_links <= 5:
-    torque_max = 200 * n_links  # ...for Planar Manipulator problem....
-else:
-    torque_max = 500 * n_links
-
-n_features = 4#2*n_links
-n_actions = 3
-tree_id = 0
-fine_tuned_simulations = False
+n_features = 4
 
 #...Loading Tree...
-#data_name = 'DS1' + '_modified'
-#env_name = 'LunarLander-v2'
-#env_name = 'Ford1DEnv'
-#env_name = 'Acrobot-v1'
-#data_name = 'LunarLander-v2_10000' + '_balanced_data'
-#data_name = 'LunarLander-v2_tau10_10000'# + '_balanced_data'
-#data_name = 'LunarLander-v2_q_values_confident_00_10000'
-#data_name = 'Acrobot-v1_10000'# + '_balanced'
-#data_name = 'PlanarManipulator_10000' + '_balanced_data'
-#data_name = 'PlanarManipulator_5_10000' + '_balanced_data'
-#data_name = 'PlanarManipulator_' + str(n_links) + '_' + str(torque_max) + '_10000' + '_balanced_data'
-#data_name = 'ford_data_new_merged'
-#data_name = 'CartPole-v0_10000'
-#data_name = 'Ford1DEnv_10000'
-#data_name = 'MountainCar'# + '_balanced'
 data_name = 'iris'
-#data_name = 'DS3_matlab'
-#testing_data_name = 'MountainCar_testing'
-#testing_data_name = 'iris'
-#testing_data_name = 'LunarLander-v2_testing_10000'
-#testing_data_name = 'Acrobot-v1_testing_10000'
-#testing_data_name = 'PlanarManipulator_testing_10000'
-#testing_data_name = 'PlanarManipulator_' + str(5) + '_testing_10000'
-#testing_data_name = 'PlanarManipulator_' + str(n_links) + '_' + str(torque_max) + '_testing_10000'
 testing_data_name = data_name
-#testing_data_name = 'CartPole-v0_testing_10000'
-#testing_data_name = 'Ford1DEnv_testing_10000'
-extra_name = '_dt_' + str(tree_id)
-#extra_name = '_classical' + extra_name
-#extra_name = '_classical' + '_simple' + extra_name
-#extra_name = '_rga' + '_simple' + extra_name
-#extra_name = '_rga' + extra_name
-tree_name = data_name + extra_name + '_pruned'
-#tree_name = 'LunarLander_balanced_data_dt_0' + '_pruned'
-#tree_name = env_name + '_fine_tuned_from_env_depth_2'# + '_' + str(tree_id)
-#tree_name = env_name + '_fine_tuned_from_data_depth_6' + '_' + str(tree_id)
 
-env_name = 'PlanarManipulator' + '_' + str(n_links) + '_' + str(torque_max)
-
-if fine_tuned_simulations:
-    tree_name = env_name + '_fine_tuned_from_env_depth_3' + extra_name
 tree_name = 'iris_classical_dt_0'
 my_tree_loc = os.path.join('..','results', data_name,tree_name + '.p')
 my_tree = pickle.load(open(my_tree_loc, 'rb'))
@@ -72,7 +25,6 @@ my_data = pd.read_csv(data_file, header=None)
 
 testing_data_file = os.path.join('..','datasets', testing_data_name + '.data')#data_file
 testing_data = pd.read_csv(testing_data_file, header = None)
-
 
 #...Data preparation...
 features = my_data.iloc[:,:n_features]
@@ -90,7 +42,7 @@ testing_data_X = my_tree.normalize_features(testing_data_X)
 
 #..main code....
 
-t1_ =  time.time()
+t1_ = time.time()
 my_tree.data_distribution_in_tree_from_c_label(features= training_data_X,
                                                class_array = action_array)
 
@@ -111,9 +63,9 @@ print('Total Active Nodes = %d' % my_tree.tree['n_active_nodes'])
 my_tree.write_text_to_tree(detailed_text = True,
                            colored_active_nodes = True)
 
-file_name = os.path.join('..','..', 'results', 'tree_plots','tree_plot')
+file_name = os.path.join('..', 'results', 'tree_plots','tree_plot')
 
-file_name_table = os.path.join('..','..', 'results', 'tree_plots', 'table_latex.tex')
+file_name_table = os.path.join('..', 'results', 'tree_plots', 'table_latex.tex')
 
 visualize_tree.plot(filename=file_name, tree = my_tree.tree)
 my_tree.write_tree_table_latex(file_name_table)
